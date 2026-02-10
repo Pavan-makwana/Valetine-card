b3022449afdd2c44fae749f6c008b7d5de3eda73
Pavan <pavan561981@gmail.com>
2026-02-10 11:28:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function nextPage() {
+    window.location.href = "yes.html";
+}
+
+function moveButton() {
+    let noButton = document.getElementById("noButton");
+    let x = Math.random() * (window.innerWidth - noButton.clientWidth);
+    let y = Math.random() * (window.innerHeight - noButton.clientHeight);
+    noButton.style.position = "absolute";
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
+}
